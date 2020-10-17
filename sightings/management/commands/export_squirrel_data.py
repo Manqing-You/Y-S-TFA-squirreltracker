7cbaff1659232d6a0df678a8ae3ef3b0763462d9
nancysunnyyy <nancysunnyyy@users.noreply.github.com>
2020-10-17 15:51:35 -0400

    export data final edition

--- sightings/management/commands/export_squirrel_data.py
+++ sightings/management/commands/export_squirrel_data.py
@@ -6,22 +6,22 @@ from django.db import models
 import csv
 
 class Command(BaseCommand):
-  help = 'Update the database of the day every morning'
+  help = 'Export the database'
 
   # 接收参数
   def add_arguments(self, parser):
-      parser.add_argument('path', type=str, help='文件路径')
+      parser.add_argument('path', type=str, help='file path')
 
   def handle(self, *args, **options):
-      path = options['path']  # 拿到参数的值
-      out = open(path, 'a', newline='') #准备并打开 要存入数据的文件对象
+      path = options['path']
+      out = open(path, 'a', newline='')
 
-      ob = Squirrel.objects.all() #获取squirrel表的全部数据对象
-      lis = ob.values_list() #获取所有数据对象的值列表
-      fields = Squirrel.getattrlist() #获取属性名列表
+      ob = Squirrel.objects.all()
+      lis = ob.values_list()
+      fields = Squirrel.getattrlist()
 
-      csv_write = csv.writer(out, dialect='excel') #以Excel为模板
-      csv_write.writerow(fields) #将字段名写入文件
+      csv_write = csv.writer(out, dialect='excel')
+      csv_write.writerow(fields)
       for i in lis:
           csv_write.writerow(i)
 

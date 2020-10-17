fc0fd5a246bc31ddcb4736fd802225e1bbe03777
nancysunnyyy <55214619+nancysunnyyy@users.noreply.github.com>
2020-10-17 00:08:13 -0400

    complete import_squirrel_data edition 2

--- sightings/management/commands/import_squirrel_data.py
+++ sightings/management/commands/import_squirrel_data.py
@@ -17,3 +17,39 @@ class Command(BaseCommand):
       print(path)
       with open(path) as fp:
           data = list(csv.DictReader(fp))
+  def testboolean(str_):
+              if str_ == 'true':
+                  return 1
+              else:
+                  return 0
+		              for i in data:
+              detester = i['Date']
+              date = datetime.datetime.strptime(detester,'%m%d%Y')
+              #print(date)
+              squirrel = sightings()
+              squirrel.Latitude = i["Y"]
+              squirrel.Longitude = i["X"]
+              squirrel.Unique_Squirrel_ID = i["Unique Squirrel ID"]
+              squirrel.Shift = i['Shift']
+              squirrel.Date = date
+              squirrel.Age = i['Age']
+              squirrel.Primary_Fur_Color = i['Primary Fur Color']
+              squirrel.Location = i['Location']
+              squirrel.Specific_Location = i['Specific Location']
+              squirrel.Running = testboolean(i['Running'])
+              #print(i['Running'])
+              #print(squirrel.Running)
+              squirrel.Chasing = testboolean(i['Chasing'])
+              squirrel.Climbing = testboolean(i['Climbing'])
+              squirrel.Eating = testboolean(i['Eating'])
+              squirrel.Foraging = testboolean(i['Foraging'])
+              squirrel.Other_Activities = i['Other Activities']
+              squirrel.Kuks = testboolean(i['Kuks'])
+              squirrel.Quaas = testboolean(i['Quaas'])
+              squirrel.Moans = testboolean(i['Moans'])
+              squirrel.Tail_flags = testboolean(i['Tail flags'])
+              squirrel.Tail_twitches = testboolean(i['Tail twitches'])
+              squirrel.Approaches = testboolean(i['Approaches'])
+              squirrel.Indifferent = testboolean(i['Indifferent'])
+              squirrel.Runs_from = testboolean(i['Runs from'])
+              squirrel.save() #保存到数据库

cfe226858d119cbca51016725c580bec0b6646c3
Manqing You <my2692@columbia.edu>
2020-10-18 04:11:52 +0000

    Fixed the same ID situation bug and Modified the import file

--- sightings/management/commands/import_squirrel_data.py
+++ sightings/management/commands/import_squirrel_data.py
@@ -22,31 +22,37 @@ class Command(BaseCommand):
                   return 1
               else:
                   return 0
+          id_=[] 
+         
           for i in data:
-              detester = i['Date']
-              date = datetime.datetime.strptime(detester,'%m%d%Y')
-              squirrel = sightings()
-              squirrel.Latitude = i["Y"]
-              squirrel.Longitude = i["X"]
-              squirrel.Unique_Squirrel_ID = i["Unique Squirrel ID"]
-              squirrel.Shift = i['Shift']
-              squirrel.Date = date
-              squirrel.Age = i['Age']
-              squirrel.Primary_Fur_Color = i['Primary Fur Color']
-              squirrel.Location = i['Location']
-              squirrel.Specific_Location = i['Specific Location']
-              squirrel.Running = testboolean(i['Running'])
-              squirrel.Chasing = testboolean(i['Chasing'])
-              squirrel.Climbing = testboolean(i['Climbing'])
-              squirrel.Eating = testboolean(i['Eating'])
-              squirrel.Foraging = testboolean(i['Foraging'])
-              squirrel.Other_Activities = i['Other Activities']
-              squirrel.Kuks = testboolean(i['Kuks'])
-              squirrel.Quaas = testboolean(i['Quaas'])
-              squirrel.Moans = testboolean(i['Moans'])
-              squirrel.Tail_flags = testboolean(i['Tail flags'])
-              squirrel.Tail_twitches = testboolean(i['Tail twitches'])
-              squirrel.Approaches = testboolean(i['Approaches'])
-              squirrel.Indifferent = testboolean(i['Indifferent'])
-              squirrel.Runs_from = testboolean(i['Runs from'])
-              squirrel.save() 
+              if i.get("Unique Squirrel ID") in id_:
+                  continue
+              else:
+                detester = i['Date']
+                date = datetime.datetime.strptime(detester,'%m%d%Y')
+                squirrel = sightings()
+                squirrel.Latitude = i["Y"]
+                squirrel.Longitude = i["X"]
+                squirrel.Unique_Squirrel_ID = i["Unique Squirrel ID"]
+                squirrel.Shift = i['Shift']
+                squirrel.Date = date
+                squirrel.Age = i['Age']
+                squirrel.Primary_Fur_Color = i['Primary Fur Color']
+                squirrel.Location = i['Location']
+                squirrel.Specific_Location = i['Specific Location']
+                squirrel.Running = testboolean(i['Running'])
+                squirrel.Chasing = testboolean(i['Chasing'])
+                squirrel.Climbing = testboolean(i['Climbing'])
+                squirrel.Eating = testboolean(i['Eating'])
+                squirrel.Foraging = testboolean(i['Foraging'])
+                squirrel.Other_Activities = i['Other Activities']
+                squirrel.Kuks = testboolean(i['Kuks'])
+                squirrel.Quaas = testboolean(i['Quaas'])
+                squirrel.Moans = testboolean(i['Moans'])
+                squirrel.Tail_flags = testboolean(i['Tail flags'])
+                squirrel.Tail_twitches = testboolean(i['Tail twitches'])
+                squirrel.Approaches = testboolean(i['Approaches'])
+                squirrel.Indifferent = testboolean(i['Indifferent'])
+                squirrel.Runs_from = testboolean(i['Runs from'])
+                squirrel.save()
+                id_.append(i.get("Unique Squirrel ID"))
